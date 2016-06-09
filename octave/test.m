10739e4c7c42d4a922ca35cb3306903e5d2acc3d
Jesse Malone <jmalone@pivotal.io>
2016-06-09 09:16:23 -0400

    WIP - use a built in octave minimizer for hypothesis finding

--- octave/test.m
+++ octave/test.m
@@ -15,3 +15,29 @@ text(X(:,2),X(:,3),X(:,4),strings)
 xlabel("alphabet cardinality")
 ylabel("vowels:consonants")
 zlabel("caps:lower")
+
+[m,n] = size(X(:,2:4))
+
+X = [ones(m, 1) X(:,2:4)];
+
+initial_theta = zeros(n + 1, 1);
+
+[cost, grad] = cost(initial_theta, X, y);
+
+fprintf('Cost at initial theta (zeros): %f\n', cost);
+fprintf('Gradient at initial theta (zeros): \n'); 
+fprintf(' %f \n', grad); 
+fprintf('\nProgram paused. Press enter to continue.\n');
+pause;
+options = optimset('GradObj', 'on', 'MaxIter', 400);
+
+[theta,cost] = fminunc(@(t)(cost(t, X, y)), initial_theta, options);
+
+fprintf('Cost at theta found by fminunc: %f\n', cost);
+fprintf('theta: \n'); 
+fprintf(' %f \n', theta);
+
+fprintf('\nProgram paused. Press enter to continue.\n'); 
+pause;
+
+
